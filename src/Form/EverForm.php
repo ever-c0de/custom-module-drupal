c1365c5aa4d80c02a4a25d228a9dfd47529042af
ever-c0de <serhii.lavryn9nko@gmail.com>
2020-11-16 14:10:22 +0200

    don't working for now

--- src/Form/EverForm.php
+++ src/Form/EverForm.php
@@ -1,16 +1,7 @@
 <?php
-/**
- * @file
- * Contains \Drupal\ever\Form\EverForm.
- *
- * В комментарии выше указываем, что содержится в данном файле.
- */
 
-// Объявляем пространство имён формы. Drupal\НАЗВАНИЕ_МОДУЛЯ\Form
 namespace Drupal\ever\Form;
 
-// Указываем что нам потребуется FormBase, от которого мы будем наследоваться
-// а также FormStateInterface который позволит работать с данными.
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Ajax\AjaxResponse;
@@ -18,40 +9,35 @@ use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Ajax\RedirectCommand;
 use Drupal\file\Entity\File;
 
-
 /**
- * Объявляем нашу форму, наследуясь от FormBase.
- * Название класса строго должно соответствовать названию файла.
+ * @file
+ * Contains \Drupal\ever\Form\EverForm.
+ *
  */
+
 class EverForm extends FormBase {
 
   /**
-   * То что ниже - это аннотация. Аннотации пишутся в комментариях и в них
-   * объявляются различные данные. В данном случае указано, что документацию
-   * к данному методу надо взять из комментария к самому классу.
+   * {@inheritDoc}.
    *
-   * А в самом методе мы возвращаем название нашей формы в виде строки.
-   * Эта строка используется для альтера формы (об этом ниже в тексте).
-   *
-   * {@inheritdoc}.
    */
+
   public function getFormId() {
     return 'ever_form';
   }
 
-  /**e
-   * Создание нашей формы.
+  /**
    *
    * {@inheritdoc}.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $form['name'] = array(
+    $form['name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your name'),
       '#maxlength' => 100,
       '#required' => TRUE,
-    );
+    ];
 
     $form['email'] = [
       '#type' => 'email',
@@ -59,49 +45,49 @@ class EverForm extends FormBase {
       '#required' => TRUE,
     ];
 
-    $form['phone_number'] = array(
+    $form['phone_number'] = [
       '#type' => 'tel',
       '#title' => $this->t('Your phone number'),
       '#required' => TRUE,
-    );
+    ];
 
 
-    $form['comment'] = array(
+    $form['comment'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Your comment'),
       '#resizable' => FALSE,
       '#cols' => 10,
       '#rows' => 4,
-    );
+    ];
 
-    $form['avatar_file'] = array(
+    $form['avatar_file'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('Your avatar photo'),
       '#description'  => t('Allowed extensions: png jpg jpeg'),
       '#upload_location' => 'public://images/avatar/',
       '#required' => FALSE,
       '#multiple' => FALSE,
-      '#upload_validators' => array(
-        'file_validate_extensions' => array('png jpg jpeg'),
-        'file_validate_size' => array(2097152),
-    ),
-    );
+      '#upload_validators' => [
+        'file_validate_extensions' => ['png jpg jpeg'],
+        'file_validate_size' => [2097152],
+      ],
+    ];
 
-    $form['comment_photo'] = array(
+    $form['comment_photo'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('Your comment photo'),
       '#description'  => t('Allowed extensions: png jpg jpeg'),
       '#upload_location' => 'public://images/photos/',
       '#required' => FALSE,
       '#multiple' => FALSE,
-      '#upload_validators'  => array(
-        'file_validate_extensions' => array('png jpg jpeg'),
-        'file_validate_size' => array(5242880),
-      ),
-    );
+      '#upload_validators'  => [
+        'file_validate_extensions' => ['png jpg jpeg'],
+        'file_validate_size' => [5242880],
+      ],
+    ];
 
     $form['actions']['#type'] = 'actions';
-    $form['actions']['submit'] = array(
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Send'),
       '#button_type' => 'primary',
@@ -112,8 +98,8 @@ class EverForm extends FormBase {
           'type' => 'throbber',
         ],
       ],
-    );
-    # Добавляем в форму новый элемент где будем выводить системные сообщения.
+    ];
+
     $form['system_messages'] = [
       '#markup' => '<div id="form-system-messages"></div>',
       '#weight' => -100,
@@ -126,21 +112,23 @@ class EverForm extends FormBase {
    */
   public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
     $ajax_response = new AjaxResponse();
-    $message = [
-      '#theme' => 'status_messages',
-      '#message_list' => drupal_get_messages(),
-      '#status_headings' => [
-        'status' => t('Status message'),
-        'error' => t('Error message'),
-        'warning' => t('Warning message'),
-      ],
-    ];
-    $messages = \Drupal::service('renderer')->render($message);
-    $ajax_response->addCommand(new HtmlCommand('#form-system-messages', $messages));
-//    if (!count($form_state->getErrors())) {
-//      $ajax_response->addCommand(new RedirectCommand('http://ever.loc/module-page'));
-//    }
-
+    if (\Drupal::messenger()->messagesByType('error') === 0) {
+      \Drupal::messenger()->deleteByType('error');
+      $ajax_response->addCommand(new RedirectCommand('http://ever.loc/module-page'));
+    }
+    else {
+      $message = [
+        '#theme' => 'status_messages',
+        '#message_list' => \Drupal::messenger()->all(),
+        '#status_headings' => [
+          'status' => t('Status message'),
+          'error' => t('Error message'),
+          'warning' => t('Warning message'),
+        ],
+      ];
+      $messages = \Drupal::service('renderer')->render($message);
+      $ajax_response->addCommand(new HtmlCommand('#form-system-messages', $messages));
+    }
     return $ajax_response;
   }
 
@@ -151,11 +139,9 @@ class EverForm extends FormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     if (strlen($form_state->getValue('name')) < 2) {
-      \Drupal::messenger()->addMessage('Name is too short.');
+      \Drupal::messenger()->addError('Name is too short.');
     }
-//    if ($form_state->getValue('email') == !\Drupal::service('email.validator')->isValid($form_state->getValue('email'))) {
-//      drupal_set_message($this->t('Sorry, you need to input valid email'));
-//    }
+
   }
 
   /**
@@ -165,19 +151,18 @@ class EverForm extends FormBase {
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    \Drupal::messenger()->addMessage('hello');  /*->t('Thank you @name, your answer was send!', array(
-      '@name' => $form_state->getValue('name'),*/
-
+    \Drupal::messenger()->addMessage($this->t('Thank you for feedback, @name',
+      ['@name' => $form_state->getValue('name')]));
 
     $photo = $form_state->getValue('avatar_file');
-    $file = File::load( $photo[0] );
+    $file = File::load($photo[0]);
     $file->setPermanent();
     $file->save();
 
     $photo = $form_state->getValue('comment_photo');
-    $file = File::load( $photo[0] );
+    $file = File::load($photo[0]);
     $file->setPermanent();
     $file->save();
-
   }
+
 }
