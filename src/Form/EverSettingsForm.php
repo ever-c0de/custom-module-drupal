2fad446a67e64b0f7a113f1ae7946a40faccd7d9
ever-c0de <serhii.lavryn9nko@gmail.com>
2020-12-03 10:36:23 +0200

    Delete useless coments ✂ ️🖋

--- src/Form/EverSettingsForm.php
+++ src/Form/EverSettingsForm.php
@@ -19,9 +19,6 @@ class EverSettingsForm extends ConfigFormBase
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    // Возвращает названия конфиг файла.
-    // Значения будут храниться в файле:
-    // ever.ever_form.settings.yml
     return [
       'ever.ever_form.settings',
     ];
@@ -31,16 +28,12 @@ class EverSettingsForm extends ConfigFormBase
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    // Загружаем наши конфиги.
     $config = $this->config('ever.ever_form.settings');
-    // Добавляем поле для возможности задать телефон по умолчанию.
-    // Далее мы будем использовать это значение в предыдущей форме.
     $form['default_phone_number'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Default phone number'),
       '#default_value' => $config->get('phone_number'),
     );
-    // Субмит наследуем от ConfigFormBase
     return parent::buildForm($form, $form_state);
   }
 
@@ -49,7 +42,6 @@ class EverSettingsForm extends ConfigFormBase
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $values = $form_state->getValues();
-    // Записываем значения в наш конфиг файл и сохраняем.
     $this->config('ever.ever_form.settings')
       ->set('phone_number', $values['default_phone_number'])
       ->save();
